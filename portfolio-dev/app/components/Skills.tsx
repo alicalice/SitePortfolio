54896a1705be3946b04af4e5e8fa190c0ef13a06
alicalice <sofialice.ds@gmail.com>
2026-05-06 11:26:42 -0300

    mexi no css e alterei o visual das tags de habilidades. não está do jeito que quero, mas é um começo

--- portfolio-dev/app/components/Skills.tsx
+++ portfolio-dev/app/components/Skills.tsx
@@ -8,18 +8,9 @@ export default function Skills(){
         <section className="mt-10 max-w-6xl mx-auto px-6 py-16" id="skills">
             <h2 className="text-brand-strong text-3xl font-semibold mb-8">Skills</h2>
             
-            <div className="flex
-            flex-wrap gap-4
-            ">
+            <div className="flex flex-wrap gap-4">
                     {skills.map((skill)=>(
-                    <div key={skill} className="px-4 py-2 
-                    bg-purple-500/10 
-                    rounded-lg 
-                    border 
-                    border-purple-300 
-                    hover:bg-purple-500/20 
-
-                    ">
+                    <div key={skill} className="skill-tag px-4 py-2">
                         {skill}
                     </div>
                 ))}
@@ -28,10 +19,3 @@ export default function Skills(){
     
     )
 }
-
-{/*text-xs
-                bg-purple-500/10
-                text-purple-300 border
-                border-purple-500/20
-                px-2 py-1
-                rounded-md*/}
